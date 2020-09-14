9adf236451e9643bd936f1f4bdb34a620a4cc374
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:16:13 +0300

    first commit

---
+++ LoginFormRefactornal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoginFormRefactornal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Login,Password;
+            Console.WriteLine("Logn: ");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password ");
+            Password = Console.ReadLine();
+
+            if(Login == "admin" && Password == "pass1234")
+            {
+                Console.WriteLine("Welcome,Admin!");
+            }
+            else
+            {
+                Console.WriteLine("Login failed.Try again.");
+            }
+
+
+        }
+    }
+}
